acfe220f4b074707e169f4801dc8612e12a5a669
Raj Botre <rajbotre2@gmail.com>
2025-03-31 11:58:10 +0530

    Channels in GO Language

---
+++ Channels/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	//fmt.Println("Channels in Golang")
+
+	myCh := make(chan int, 1) //second parameter is buffer -Number of channels
+	wg := &sync.WaitGroup{}
+
+	// myCh <- 5
+	// fmt.Println(<-myCh)
+	wg.Add(2)
+	//Recieve only
+	go func(ch <-chan int, wt *sync.WaitGroup) {
+		//close(myCh)
+		val, isChanelOpen := <-myCh
+		fmt.Println(isChanelOpen)
+		fmt.Println(val)
+
+		//fmt.Println(<-myCh)
+		//fmt.Println(<-myCh)
+		wg.Done()
+
+	}(myCh, wg)
+	//Send only
+	go func(ch chan<- int, wt *sync.WaitGroup) {
+
+		myCh <- 5
+		// myCh <- 6
+		close(myCh)
+		wg.Done()
+	}(myCh, wg)
+
+	wg.Wait()
+}
