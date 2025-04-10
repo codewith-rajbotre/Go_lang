bfd7981632c886ad70e239e9352f400d1b5a53c1
Raj Botre <rajbotre2@gmail.com>
2025-04-10 21:29:30 +0530

    Slice of string

--- GO_Detailed/MapArray/SliceArray.go
+++ GO_Detailed/MapArray/SliceArray.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	var intSlice []int32 = []int32{4, 5, 6}
+	var stringSlice []string = []string{"Mandar", "Manoj", "Gopal"}
+	fmt.Println(stringSlice)
 	fmt.Println(intSlice) //[4,5,6]
 	intSlice = append(intSlice, 7)
 	fmt.Println(intSlice) //[4,5,6,7]
