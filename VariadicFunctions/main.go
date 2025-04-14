a9521dc9f084b240a90b7411e9a5e3c9c5987cb0
Raj Botre <rajbotre2@gmail.com>
2025-04-14 22:40:26 +0530

    Variadic Functions

--- VariadicFunctions/main.go
+++ VariadicFunctions/main.go
@@ -12,11 +12,20 @@ func sum(nums ...int) {
 	fmt.Println(total)
 }
 
+func sub(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, number := range nums {
+		total -= number
+	}
+	fmt.Println(total)
+}
+
 func main() {
 
 	sum(1, 2)
 	sum(1, 2, 3)
-
+	sub(7, 5, 3, 2)
 	nums := []int{1, 2, 3, 4}
 	sum(nums...)
 }
